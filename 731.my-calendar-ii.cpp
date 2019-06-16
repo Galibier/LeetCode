7c3ccfd14d425e5b6a06e788241146db3a65ac70
Galibier <cx.sjtu@gmail.com>
2019-06-17 00:08:08 +0800

    ..

---
+++ 731.my-calendar-ii.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=731 lang=cpp
+ *
+ * [731] My Calendar II
+ */
+class MyCalendarTwo {
+public:
+    MyCalendarTwo() {
+        
+    }
+    
+    bool book(int start, int end) {
+        
+    }
+};
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo* obj = new MyCalendarTwo();
+ * bool param_1 = obj->book(start,end);
+ */
+
