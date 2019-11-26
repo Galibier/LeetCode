3e69f939bf0d346480d10e55b9e53e4772b90faa
Galibier <cx.sjtu@gmail.com>
2019-11-26 22:51:29 +0800

    median-of-two-sorted-arrays

--- 4.median-of-two-sorted-arrays.cpp
+++ 4.median-of-two-sorted-arrays.cpp
@@ -3,30 +3,42 @@
  *
  * [4] Median of Two Sorted Arrays
  */
+const int MAX_INT = std::numeric_limits<int>::max();
+const int MIN_INT = std::numeric_limits<int>::min();
+
 class Solution {
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)
+    double findMedianSortedArrays(const std::vector<int>& v1,
+        const std::vector<int>& v2)
     {
-        int m = nums1.size(), n = nums2.size(), l = 0, r = m;
-        if (m > n) {
-            return findMedianSortedArrays(nums2, nums1);
-        }
-        while (l <= r) {
-            int i = (l + r) / 2, j = (m + n + 1) / 2 - i;
-            if (i && nums1[i - 1] > nums2[j]) {
-                r = i - 1;
-            } else if (i < m && nums2[j - 1] > nums1[i]) {
-                l = i + 1;
+        // Let A.size() <= B.size()
+        const auto& A = (v1.size() < v2.size() ? v1 : v2);
+        const auto& B = (v1.size() < v2.size() ? v2 : v1);
+        const int m = A.size();
+        const int n = B.size();
+
+        // Pad space between numbers
+        // 0 1 2 3 4 5 6
+        // # 0 # 1 # 2 #
+        // Left index = (Cut - 1) / 2
+        // Right index = Cut / 2
+        int L = 0, R = m * 2 + 1;
+        while (L < R) {
+            int C1 = (L + R) / 2;
+            int C2 = m + n - C1;
+
+            double L1 = (C1 == 0) ? MIN_INT : A[(C1 - 1) / 2];
+            double L2 = (C2 == 0) ? MIN_INT : B[(C2 - 1) / 2];
+            double R1 = (C1 == m * 2) ? MAX_INT : A[C1 / 2];
+            double R2 = (C2 == n * 2) ? MAX_INT : B[C2 / 2];
+            if (L1 > R2) {
+                R = C1;
+            } else if (L2 > R1) {
+                L = C1 + 1;
             } else {
-                int lmax = !i ? nums2[j - 1] : (!j ? nums1[i - 1] : max(nums1[i - 1], nums2[j - 1]));
-                if ((m + n) % 2) {
-                    return lmax;
-                }
-                int rmin = i == m ? nums2[j] : (j == n ? nums1[i] : min(nums1[i], nums2[j]));
-                return 0.5 * (lmax + rmin);
+                return (std::max(L1, L2) + std::min(R1, R2)) / 2.0;
             }
         }
-        return 0.0;
+        return -1;
     }
 };
-
